edf327336e7382da2e2bf0e8e58eb9014de54928
Neha991318 <neha.d.desai9913@gmail.com>
2025-06-11 09:47:51 -0400

    favourites model

---
+++ src/app/models/Favourites.ts
@@ -0,0 +1,10 @@
+// models/Favorite.ts
+import mongoose from 'mongoose';
+
+const favoriteSchema = new mongoose.Schema({
+  user:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  recipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe', required: true },
+  savedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.Favorite || mongoose.model('Favorite', favoriteSchema);
